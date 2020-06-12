3d94ae7edd502b1275a2b84564610b98cf667b09
Calvin Au <cal.au0927@gmail.com>
2020-06-12 19:01:39 +0800

    [+] Add basic redux

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Provider as ReduxProvider} from 'react-redux';
+import store from '../redux/store';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <ReduxProvider store={store}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
+    </ReduxProvider>
   );
 }
 
---
+++ src/redux/store.ts
@@ -0,0 +1,7 @@
+import {createStore, combineReducers} from 'redux';
+
+const AppReducer = combineReducers({});
+
+const store = createStore(AppReducer);
+
+export default store;
